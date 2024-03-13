51c93c6222a052a7aa02a5bf918c8ae98facc812
Daniel del Río <daniel.delrio.juncal@gmail.com>
2024-03-13 20:58:24 +0100

    See temperature

---
+++ src/app/modules/main/main-container/sensor-data/summary-data/summary-data-card/summary-data-card.component.ts
@@ -0,0 +1,60 @@
+import { Component, input } from '@angular/core';
+import { RegisteredSensor } from '../../../../../../shared/models/registered-sensor.model';
+import { AsyncPipe, DatePipe, DecimalPipe, NgIf } from '@angular/common';
+import {
+    Measurement,
+    MeasurementService,
+} from '../../../../../../http/devices/measurement.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { combineLatest, filter, Observable, startWith, switchMap } from 'rxjs';
+import { SensorDataFormService } from '../../sensor-data-form.service';
+import { MatTooltip } from '@angular/material/tooltip';
+
+@Component({
+    selector: 'app-summary-data-card',
+    standalone: true,
+    imports: [DecimalPipe, AsyncPipe, NgIf, MatTooltip, DatePipe],
+    templateUrl: './summary-data-card.component.html',
+})
+export class SummaryDataCardComponent {
+    device = input<RegisteredSensor>();
+    minMaxMeasurements$: Observable<Measurement[]>;
+
+    constructor(
+        private readonly _sensorDataFormService: SensorDataFormService,
+        private readonly _measurementService: MeasurementService,
+    ) {
+        this.minMaxMeasurements$ = combineLatest([
+            toObservable(this.device).pipe(filter((device) => device != null)),
+            this._sensorDataFormService.range.valueChanges.pipe(
+                startWith(this._sensorDataFormService.range.value),
+                filter(() => this._sensorDataFormService.range.valid),
+            ),
+        ]).pipe(
+            switchMap(([device, range]) => {
+                const endDate = range.end;
+                endDate?.setHours(23, 59, 59, 999);
+                return combineLatest([
+                    this._measurementService.getMaxByDeviceAndDates(
+                        device!.id,
+                        range.start?.getTime() != null
+                            ? Math.round(range.start?.getTime() / 1000)
+                            : undefined,
+                        endDate?.getTime() != null
+                            ? Math.round(endDate?.getTime() / 1000)
+                            : undefined,
+                    ),
+                    this._measurementService.getMinByDeviceAndDates(
+                        device!.id,
+                        range.start?.getTime() != null
+                            ? Math.round(range.start?.getTime() / 1000)
+                            : undefined,
+                        endDate?.getTime() != null
+                            ? Math.round(endDate?.getTime() / 1000)
+                            : undefined,
+                    ),
+                ]);
+            }),
+        );
+    }
+}
