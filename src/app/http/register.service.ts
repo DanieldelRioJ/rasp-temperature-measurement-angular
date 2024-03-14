731f95662a6fc92c150a85a59b02a704a4f1adf9
Daniel del Río <daniel.delrio.juncal@gmail.com>
2024-03-14 01:09:24 +0100

    Register

--- src/app/http/register.service.ts
+++ src/app/http/register.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { backUrl } from '../../environments/environment';
+import { AuthService } from '../core/services/auth.service';
+import { tap } from 'rxjs';
+import { LoginResponse } from './login/login.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class RegisterService {
-    constructor(private readonly _httpClient: HttpClient) {}
+    constructor(
+        private readonly _httpClient: HttpClient,
+        private readonly _authService: AuthService,
+    ) {}
+
+    register(email: string, token: string, password: string) {
+        return this._httpClient
+            .post<LoginResponse>(`${backUrl}/register`, {
+                email,
+                token,
+                password,
+            })
+            .pipe(
+                tap((response) => {
+                    this._authService.user.set(response.data);
+                    this._authService.token = response.token;
+                }),
+            );
+    }
 
     invite(email: string, role: string) {
         return this._httpClient.post(`${backUrl}/invite`, {
--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import {
+    MatError,
     MatFormField,
     MatLabel,
     MatPrefix,
 } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import {
+    FormBuilder,
+    FormControl,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { FormErrorDirective } from '../../../shared/form-error/form-error.directive';
+import { RegisterService } from '../../../http/register.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NotificationService } from '../../../shared/notification/notification.service';
 
 @Component({
     selector: 'app-register',
@@ -20,7 +31,66 @@ import { RouterLink } from '@angular/router';
         MatLabel,
         MatPrefix,
         RouterLink,
+        ReactiveFormsModule,
+        FormErrorDirective,
+        MatError,
     ],
     templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+    @Input() email?: string;
+    @Input() token?: string;
+
+    registerForm = this._formBuilder.group({
+        email: new FormControl<string>('', [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.email,
+        ]),
+        token: new FormControl<string>('', [
+            Validators.required,
+            Validators.minLength(1),
+        ]),
+        password: new FormControl<string>('', [
+            Validators.required,
+            Validators.minLength(8),
+        ]),
+    });
+
+    private _destroyRef = inject(DestroyRef);
+
+    constructor(
+        private readonly _formBuilder: FormBuilder,
+        private readonly _registerService: RegisterService,
+        private readonly _notificationService: NotificationService,
+        private readonly _router: Router,
+    ) {}
+
+    ngOnInit(): void {
+        this.registerForm.patchValue({ email: this.email, token: this.token });
+    }
+
+    register() {
+        const formValue = this.registerForm.value;
+        this._registerService
+            .register(formValue.email!, formValue.token!, formValue.password!)
+            .pipe(takeUntilDestroyed(this._destroyRef))
+            .subscribe({
+                next: () => {
+                    this._notificationService.send(
+                        'Cuenta creada con éxito',
+                        null,
+                        'success',
+                    );
+                    this._router.navigateByUrl('/');
+                },
+                error: () => {
+                    this._notificationService.send(
+                        `Ha ocurrido un fallo en el registro`,
+                        null,
+                        'error',
+                    );
+                },
+            });
+    }
+}
